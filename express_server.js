4721d6a297cb7a6e75d170a7e6510bde6596c767
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-13 00:59:10 +0000

    Implement logout button with server/client side logic

--- express_server.js
+++ express_server.js
@@ -47,6 +47,11 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 })
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+
 //Assigns a new key value pair to urlDatabase and redirects client
 app.post("/urls", (req, res) => {
   const randomString = generateRandomString();
