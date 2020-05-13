684f36e99e8116098da6e54df114a1b66993fe28
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-13 00:00:52 +0000

    Added cookie to page webapp

--- express_server.js
+++ express_server.js
@@ -21,16 +21,25 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+  
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+app.post("/login", (req, res) => {
+  const name = req.body.username;
+  res.cookie("username", name);
+  res.redirect("/urls");
+})
 
 //Assigns a new key value pair to urlDatabase and redirects client
 app.post("/urls", (req, res) => {
@@ -65,7 +74,11 @@ app.get("/u/:shortURL", (req, res) => {
 
 //Handles get request and renders HTML with templateVars
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"],
+  };
   res.render("urls_show", templateVars);
 });
 
